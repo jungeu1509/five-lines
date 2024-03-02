ebd4d7316b5bf57d208bab7e5f904cadb2df56af
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2024-03-02 17:48:49 +0900

    [section-5.3] 복잡한 조건 통합 및 조건 산술 적용

--- index.ts
+++ index.ts
@@ -504,13 +504,10 @@ function updateMap() {
 }
 
 function updateTile(x: number, y: number) {
-  if (map[y][x].isStone()
+  if ((map[y][x].isStone() || map[y][x].isBox())
     && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Stone(new Falling());
-    map[y][x] = new Air();
-  } else if (map[y][x].isBox()
-    && map[y + 1][x].isAir()) {
-    map[y + 1][x] = new Box(new Falling());
+    map[y][x].drop();
+    map[y + 1][x] = map[y][x];
     map[y][x] = new Air();
   } else if (map[y][x].isFalling()) {
     map[y][x].rest();
