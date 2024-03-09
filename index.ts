e919751e1d611d7487af26e890d5f66b2eb44ad2
Kenneth Jung <52902010+jungeu1509@users.noreply.github.com>
2024-03-09 17:02:22 +0900

    [section-5.5] 메서드 추출

--- index.ts
+++ index.ts
@@ -374,10 +374,17 @@ function transformMap() {
 
 let inputs: Input[] = [];
 
+class RemoveStrategy {
+  check(tile: Tile) {
+    return tile.isLock1();
+  }
+}
+
 function removeLock1() {
+  let shouldRemove = new RemoveStrategy();
   for (let y = 0; y < map.length; y++) {
     for (let x = 0; x < map[y].length; x++) {
-      if (map[y][x].isLock1()) {
+      if (shouldRemove.check(map[y][x])) {
         map[y][x] = new Air();
       }
     }
